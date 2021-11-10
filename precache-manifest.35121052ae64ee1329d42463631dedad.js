1354710542b65071957d7350870cbdd17fd76923
LuisRetana <luisretana123@gmail.com>
2021-11-09 22:42:35 -0500

    Updates

---
+++ precache-manifest.35121052ae64ee1329d42463631dedad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4d162a88cbf5f3201cf256ef9b5eb0",
+    "url": "/My-React-portfolio/index.html"
+  },
+  {
+    "revision": "c313cb5512855d619e22",
+    "url": "/My-React-portfolio/static/css/main.f135b31c.chunk.css"
+  },
+  {
+    "revision": "75267617a7cd558b0c24",
+    "url": "/My-React-portfolio/static/js/2.af0d4b87.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/My-React-portfolio/static/js/2.af0d4b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6fb096d9b70d41f103d",
+    "url": "/My-React-portfolio/static/js/3.c9e9ebb7.chunk.js"
+  },
+  {
+    "revision": "c313cb5512855d619e22",
+    "url": "/My-React-portfolio/static/js/main.4883cd88.chunk.js"
+  },
+  {
+    "revision": "aad9e3d7c82b3f5b914c",
+    "url": "/My-React-portfolio/static/js/runtime-main.645d6d8b.js"
+  },
+  {
+    "revision": "ed038a5ef0f1bf800e887006abd2596e",
+    "url": "/My-React-portfolio/static/media/Luis_Retana_-_Software_Engineer (2).ed038a5e.pdf"
+  },
+  {
+    "revision": "c692d4adcd50533cb74ecf8134888182",
+    "url": "/My-React-portfolio/static/media/app-screenshot.c692d4ad.png"
+  },
+  {
+    "revision": "b80d00499e67ff5b28af195cb4dc6b07",
+    "url": "/My-React-portfolio/static/media/ecommerce.b80d0049.png"
+  },
+  {
+    "revision": "25b0cedcf8c1bcd8e28f8d4d0d07d6af",
+    "url": "/My-React-portfolio/static/media/project 1.25b0cedc.PNG"
+  },
+  {
+    "revision": "96f1e684b688d1ab596310445694e12d",
+    "url": "/My-React-portfolio/static/media/quiz-screen-shot.96f1e684.png"
+  },
+  {
+    "revision": "8574eaa23d5b1dd4d4867b938dc3360b",
+    "url": "/My-React-portfolio/static/media/recent-photo.8574eaa2.jpg"
+  },
+  {
+    "revision": "35e52aa5189df268ba1013d12216addf",
+    "url": "/My-React-portfolio/static/media/run buddy.35e52aa5.PNG"
+  },
+  {
+    "revision": "54905e9b2a53ddeb8d7842c2f3baaa62",
+    "url": "/My-React-portfolio/static/media/schedule.54905e9b.PNG"
+  },
+  {
+    "revision": "853da88dd4e77fec121a28b756720afd",
+    "url": "/My-React-portfolio/static/media/screenshot-note-taker.853da88d.png"
+  }
+]);
